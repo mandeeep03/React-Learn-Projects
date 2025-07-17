d958123679c61fecdc9ae1d0f64a7155c2661af9
mandeeep03 <devilcloie@gmail.com>
2025-07-17 18:08:35 +0530

    used counter to learn batched data send by usestate

--- Day2/Counter/src/App.jsx
+++ Day2/Counter/src/App.jsx
@@ -4,12 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let [Counter, setCounter]=useState(15)
+  let [Counter, setCounter]=useState(0)
   const addValue = () => {
     console.log('Button Clicked');
     Counter++;  
     if(Counter>10)Counter=10;  
     setCounter(Counter);
+    setCounter(Counter);
+    setCounter(Counter);
+    setCounter(Counter);
+    // all these setCounter calls are equivalent to a single setCounter call.Coz react treats all these as same and send them in a batch(only one).
+
   }
   const remValue = () => {
     console.log('Button Clicked'); 
